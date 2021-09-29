8abcefe34d69053877c2d07cfd5ae860a9e78d0f
bretathompson <brettestmail@gmail.com>
2021-09-28 18:23:38 -0600

    1st commit

---
+++ api.js
@@ -0,0 +1,9 @@
+const url_string = "https://iskarr.github.io/austindonovan.github.io/api/business.json"
+
+
+fetch(url_string)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.write(jsObject);
+    })
